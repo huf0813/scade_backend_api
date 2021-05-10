c7f8e88425c089c47e7147e13a459a6794275eac
harun ulum <student.harun0813@gmail.com>
2021-05-11 06:42:12 +0700

    init root endpoint

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"github.com/huf0813/scade_backend_api/routes"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	e := echo.New()
-
+	routes.NewRoutes(e)
 	e.Logger.Fatal(e.Start(":8009"))
 }
--- routes/routes.go
+++ routes/routes.go
@@ -1,7 +1,17 @@
 package routes
 
-import "github.com/labstack/echo/v4"
+import (
+	"github.com/huf0813/scade_backend_api/utils/custom_response"
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
 
 func NewRoutes(e *echo.Echo) {
-
+	e.GET("/", func(context echo.Context) error {
+		return context.JSON(http.StatusOK, custom_response.NewCustomResponse(
+			true,
+			"welcome to API, please contact the maintainer",
+			nil),
+		)
+	})
 }
