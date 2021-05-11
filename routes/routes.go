0b907046b8323acf69af4fe6203ce9cfe53e6507
harun ulum <student.harun0813@gmail.com>
2021-05-11 17:10:50 +0700

    inject article into routes.go

--- routes/routes.go
+++ routes/routes.go
@@ -1,6 +1,9 @@
 package routes
 
 import (
+	_articleHandler "github.com/huf0813/scade_backend_api/app/article/delivery/http"
+	_articleRepoMysql "github.com/huf0813/scade_backend_api/app/article/repository/mysql"
+	_articleUseCase "github.com/huf0813/scade_backend_api/app/article/usecase"
 	_migrationHandler "github.com/huf0813/scade_backend_api/migration/delivery/http"
 	_migrationRepoMysql "github.com/huf0813/scade_backend_api/migration/repository/mysql"
 	_migrationUseCase "github.com/huf0813/scade_backend_api/migration/usecase"
@@ -23,4 +26,8 @@ func NewRoutes(e *echo.Echo, db *gorm.DB, timeOut time.Duration) {
 	migrationRepoMysql := _migrationRepoMysql.NewMigrationRepoMysql(db)
 	migrationUseCase := _migrationUseCase.NewMigrationUseCase(migrationRepoMysql, timeOut)
 	_migrationHandler.NewMigrationHandler(e, migrationUseCase)
+
+	articleRepoMysql := _articleRepoMysql.NewArticleRepoMysql(db)
+	articleUseCase := _articleUseCase.NewArticleUseCase(articleRepoMysql, timeOut)
+	_articleHandler.NewArticleHandler(e, articleUseCase)
 }
