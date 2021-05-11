ae9d0bfbfa4f5e5db75eb970c55d7d458dd664b2
harun ulum <student.harun0813@gmail.com>
2021-05-11 16:42:25 +0700

    add inject migration domain to routes

--- routes/routes.go
+++ routes/routes.go
@@ -1,13 +1,17 @@
 package routes
 
 import (
+	_migrationHandler "github.com/huf0813/scade_backend_api/migration/delivery/http"
+	_migrationRepoMysql "github.com/huf0813/scade_backend_api/migration/repository/mysql"
+	_migrationUseCase "github.com/huf0813/scade_backend_api/migration/usecase"
 	"github.com/huf0813/scade_backend_api/utils/custom_response"
 	"github.com/labstack/echo/v4"
 	"gorm.io/gorm"
 	"net/http"
+	"time"
 )
 
-func NewRoutes(e *echo.Echo, db *gorm.DB) {
+func NewRoutes(e *echo.Echo, db *gorm.DB, timeOut time.Duration) {
 	e.GET("/", func(context echo.Context) error {
 		return context.JSON(http.StatusOK, custom_response.NewCustomResponse(
 			true,
@@ -15,4 +19,8 @@ func NewRoutes(e *echo.Echo, db *gorm.DB) {
 			nil),
 		)
 	})
+
+	migrationRepoMysql := _migrationRepoMysql.NewMigrationRepoMysql(db)
+	migrationUseCase := _migrationUseCase.NewMigrationUseCase(migrationRepoMysql, timeOut)
+	_migrationHandler.NewMigrationHandler(e, migrationUseCase)
 }
