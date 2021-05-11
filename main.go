9cdaa455cd91cf4b4120d3f0591da51a92ae3eb4
harun ulum <student.harun0813@gmail.com>
2021-05-11 08:09:27 +0700

    injection mysql_db to main

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	mysql_driver "github.com/huf0813/scade_backend_api/infra/database/mysql"
 	"github.com/huf0813/scade_backend_api/routes"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	e := echo.New()
-	routes.NewRoutes(e)
+	db, err := mysql_driver.NewDriverMysql()
+	if err != nil {
+		panic(err)
+	}
+	routes.NewRoutes(e, db)
 	e.Logger.Fatal(e.Start(":8009"))
 }
--- routes/routes.go
+++ routes/routes.go
@@ -3,10 +3,11 @@ package routes
 import (
 	"github.com/huf0813/scade_backend_api/utils/custom_response"
 	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
 	"net/http"
 )
 
-func NewRoutes(e *echo.Echo) {
+func NewRoutes(e *echo.Echo, db *gorm.DB) {
 	e.GET("/", func(context echo.Context) error {
 		return context.JSON(http.StatusOK, custom_response.NewCustomResponse(
 			true,
