9d00bff1711381d2c5f61438af29aaf8f6bc165c
harun ulum <student.harun0813@gmail.com>
2021-05-17 15:57:03 +0700

    inject article lang into routes

--- routes/routes.go
+++ routes/routes.go
@@ -4,6 +4,9 @@ import (
 	_articleHandler "github.com/huf0813/scade_backend_api/app/article/delivery/http"
 	_articleRepoMysql "github.com/huf0813/scade_backend_api/app/article/repository/mysql"
 	_articleUseCase "github.com/huf0813/scade_backend_api/app/article/usecase"
+	_articleLanguageHandler "github.com/huf0813/scade_backend_api/app/article_language/delivery/http"
+	_articleLanguageRepoMysql "github.com/huf0813/scade_backend_api/app/article_language/repository/mysql"
+	_articleLanguageUseCase "github.com/huf0813/scade_backend_api/app/article_language/usecase"
 	_migrationHandler "github.com/huf0813/scade_backend_api/migration/delivery/http"
 	_migrationRepoMysql "github.com/huf0813/scade_backend_api/migration/repository/mysql"
 	_migrationUseCase "github.com/huf0813/scade_backend_api/migration/usecase"
@@ -26,6 +29,10 @@ func NewRoutes(e *echo.Echo, db *gorm.DB, timeOut time.Duration) {
 	migrationRepoMysql := _migrationRepoMysql.NewMigrationRepoMysql(db)
 	migrationUseCase := _migrationUseCase.NewMigrationUseCase(migrationRepoMysql, timeOut)
 	_migrationHandler.NewMigrationHandler(e, migrationUseCase)
+
+	articleLanguageRepoMysql := _articleLanguageRepoMysql.NewArticleLanguageRepoMysql(db)
+	articleLanguageUseCase := _articleLanguageUseCase.NewArticleLanguageUseCase(articleLanguageRepoMysql, timeOut)
+	_articleLanguageHandler.NewArticleLanguageHandler(e, articleLanguageUseCase)
 
 	articleRepoMysql := _articleRepoMysql.NewArticleRepoMysql(db)
 	articleUseCase := _articleUseCase.NewArticleUseCase(articleRepoMysql, timeOut)
