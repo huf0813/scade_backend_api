6b6b7ea848e1ead4b275da5a233bae1f7147dbc0
harun ulum <student.harun0813@gmail.com>
2021-05-19 06:59:36 +0700

    add user model

---
+++ domain/user.go
@@ -0,0 +1,39 @@
+package domain
+
+import (
+	"context"
+	"gorm.io/gorm"
+	"time"
+)
+
+type User struct {
+	gorm.Model
+	Name     string `gorm:"not_null" json:"name"`
+	Address  string `gorm:"not_null" json:"address"`
+	Email    string `gorm:"not_null;unique" json:"email"`
+	Phone    string `gorm:"not_null;unique" json:"phone"`
+	Password string `gorm:"not_null" json:"password"`
+}
+
+type UserSignInRequest struct {
+	Email    string `json:"email" validate:"required"`
+	Password string `json:"password" validate:"required"`
+}
+
+type UserSignUpRequest struct {
+	Name     string `json:"name" validate:"required"`
+	Address  string `json:"address" validate:"required"`
+	Email    string `json:"email" validate:"required"`
+	Phone    string `json:"phone" validate:"required"`
+	Password string `json:"password" validate:"required"`
+}
+
+type UserRepository interface {
+	SignIn(ctx context.Context, email, password string) error
+	SignUp(ctx context.Context, name, address, email, phone, password string) error
+}
+
+type UserUseCase interface {
+	SignIn(ctx context.Context, email, password string, expiration int, timeType time.Duration) (string, error)
+	SignUp(ctx context.Context, name, address, email, phone, password string) error
+}
